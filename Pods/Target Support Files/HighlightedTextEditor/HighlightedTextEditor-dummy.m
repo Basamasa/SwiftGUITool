f2a6bb46e2d78ee1d956270d2bea93f5d99aa57c
Aierken Ainizhaer <aierken.ainizhaer@ubimax.com>
2021-10-25 18:42:33 +0200

    delete stuf

--- Pods/Target Support Files/HighlightedTextEditor/HighlightedTextEditor-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_HighlightedTextEditor : NSObject
-@end
-@implementation PodsDummy_HighlightedTextEditor
-@end
--- Pods/Target Support Files/Pods-SwiftGUITool-SwiftGUIToolUITests/Pods-SwiftGUITool-SwiftGUIToolUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SwiftGUITool_SwiftGUIToolUITests : NSObject
-@end
-@implementation PodsDummy_Pods_SwiftGUITool_SwiftGUIToolUITests
-@end
